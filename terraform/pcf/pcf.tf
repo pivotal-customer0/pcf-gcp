f908d71c8a448d5f51b772d28151d10b18f2a1d5
virtmerlin <merlin@sense.net>
2016-08-02 10:10:14 -0500

    UPDATE pcf.tf , added reqd var declarations

--- terraform/pcf/pcf.tf
+++ terraform/pcf/pcf.tf
@@ -1,3 +1,20 @@
+//// Declar vars
+
+variable "project" {}
+variable "region" {}
+variable "resource-prefix" {}
+variable "bosh-subnet-cidr-range" {}
+variable "zone1" {}
+variable "zone2" {}
+variable "concourse-subnet-public-cidr-range" {}
+variable "concourse-subnet-private-cidr-range" {}
+variable "pcf-subnet-private-zone1-cidr-range" {}
+variable "pcf-subnet-public-zone1-cidr-range" {}
+variable "pcf-subnet-private-zone2-cidr-range" {}
+variable "pcf-subnet-public-zone2-cidr-range" {}
+variable "sys-domain" {}
+variable "key-json" {}
+
 //// Set GCP Provider info
 
 provider "google" {
